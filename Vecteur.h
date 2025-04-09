529251ab08758831afd22ab61a32be60faaaf02e
ktafofounoum <karl.noumdem@etudiant.univ-rennes1.fr>
2025-04-09 16:01:13 +0200

    rendu TP2

--- Vecteur.h
+++ Vecteur.h
@@ -1,183 +1,175 @@
 #pragma once
-#include  <iostream>
+#include <iostream>
 #include <cassert>
 
 template<class T>
 class Vecteur{
 private:
-	T* m_tab;
-	int m_dim;
-	
+    T* donnees; // Pointeur vers le tableau stockant les composantes.
+    int dimension_vecteur; // Nombre de composantes du vecteur.
 
 public:
 
-	Vecteur(int Dimension = 3) {
-		//on verifie si la dimension est strictement superieur a zero
-		if (Dimension < 0) {
-			std::cout << "la dimension d'un vecteur doit etre superieur a zero" << std::endl;
-		}
-		assert(Dimension > 0);
+    Vecteur(int Dimension = 3) {
+        // Vérification que la dimension fournie est positive.
+        if (Dimension < 0) {
+            std::cout << "La dimension doit être strictement positive." << std::endl;
+        }
+        assert(Dimension > 0);
 
-		m_dim = Dimension;
+        dimension_vecteur = Dimension;
 
-		//ensuite on alloue un tableau dynamique
-		m_tab = new T[m_dim];
+        // Allocation dynamique du tableau pour les composantes.
+        donnees = new T[dimension_vecteur];
 
-		//ensuite on remplie le tableau avec la valeur par defaut
-		/*for (int i = 0; i < m_dim; i++) {
-			m_tab[i] = value;
-		}*/
-		//message de fin de construction
-		std::cout << "initialisation d'un vecteur a, " << m_dim << " dimensions" << std::endl;
-	}
+        // Indication de la dimension du vecteur créé.
+        std::cout << "Vecteur de " << dimension_vecteur << " dimensions initialisé." << std::endl;
+    }
 
-	template <typename U>
-	Vecteur(const Vecteur<U>& other) {
-		m_dim = other.dimensions();  // On copie la dimension
-		m_tab = new T[m_dim];         // On alloue un tableau de type T
+    template <typename U>
+    Vecteur(const Vecteur<U>& other) {
+        dimension_vecteur = other.obtenir_dimension(); // Récupération de la dimension de l'autre vecteur.
+        donnees = new T[dimension_vecteur]; // Allocation pour les nouvelles composantes.
 
-		// Conversion de chaque �l�ment de U � T
-		for (int i = 0; i < m_dim; i++) {
-			m_tab[i] = other.get(i); // Conversion explicite
-		}
-	}
+        // Copie et conversion des composantes de l'autre vecteur.
+        for (int i = 0; i < dimension_vecteur; i++) {
+            donnees[i] = other.obtenir_element(i); // Lecture de l'élément et conversion.
+        }
+    }
 
-	~Vecteur() {
-		delete[] m_tab;
-		m_tab = nullptr;
-	}
+    ~Vecteur() {
+        delete[] donnees;
+        donnees = nullptr; // Sécurité : éviter les pointeurs orphelins.
+    }
 
-	//GETTRES
+    // ACCESSEURS
 
-	const T& get(const int position) const {
-		//on verifie que la position demander est bien valide
-		assert(position < m_dim);
-		return m_tab[position];
-	}
+    const T& obtenir_element(const int position) const {
+        // Vérification de la validité de l'indice demandé.
+        assert(position < dimension_vecteur);
+        return donnees[position];
+    }
 
-	void set(const int position, T& value) {
-		//on verifie que la position que 'on sohaite modifie est bien valide
-		assert(position < m_dim);
+    void definir_element(const int position, T& valeur) {
+        // Vérification de la validité de l'indice à modifier.
+        assert(position < dimension_vecteur);
 
-		m_tab[position] = value;
-	}
+        donnees[position] = valeur;
+    }
 
-	const int dimensions(void)const {
-		return m_dim;
-	}
+    const int obtenir_dimension() const {
+        return dimension_vecteur;
+    }
 
-	Vecteur<T> operator+(const Vecteur<T>& other) const {
-		assert(this->m_dim == other.m_dim);
+    Vecteur<T> operator+(const Vecteur<T>& other) const {
+        assert(this->dimension_vecteur == other.dimension_vecteur);
 
-		Vecteur<T> result(this->m_dim);
-		for (int i = 0; i < this->m_dim; i++) {
-			result[i] = this->get(i) + other.get(i);
-		}
-		return result;
-	}
+        Vecteur<T> resultat(this->dimension_vecteur);
+        for (int i = 0; i < this->dimension_vecteur; i++) {
+            resultat[i] = this->obtenir_element(i) + other.obtenir_element(i);
+        }
+        return resultat;
+    }
 
+    // Nécessaire comme ami pour modifier les membres privés directement.
+    friend inline std::istream& operator>>(std::istream& in, Vecteur<T>& vect) {
+        // Libération de l'ancien tableau de composantes.
+        delete[] vect.donnees;
 
+        T valeur_entree;
+        int dim_entree;
+        std::cout << "Saisie des informations du vecteur." << std::endl;
 
-	//Et je me suis rendu compte qu'il fallait que cette operateur soit ami de la classe 
-	//pour poupoir acceder a sa dimension et la modifie ainsi que son tableau et liberer la m�moire
-	friend inline std::istream& operator>>(std::istream& in, Vecteur<T>& vect) {
-		//on libere d'abord le tableau de vect
-		delete[] vect.m_tab;
+        // Demande et lecture de la dimension du vecteur.
+        std::cout << "Entrez la dimension du vecteur : " << std::endl;
+        in >> dim_entree;
 
-		T value;
-		int dim;
-		std::cout << "i am here" << std::endl;
+        // Mise à jour de la dimension et allocation du nouveau tableau.
+        vect.dimension_vecteur = dim_entree;
+        vect.donnees = new T[dim_entree];
 
-		//Messsage pour demander la dim du vecteur
-		std::cout << "Entrez la dimension de votre vecteur" << std::endl;
-		in >> dim;
+        // Lecture des valeurs pour chaque composante.
+        for (int i = 0; i < dim_entree; i++) {
+            // Demande de la valeur pour la i-ème composante.
+            std::cout << "Valeur pour la composante " << i + 1 << " : " << std::endl;
+            in >> valeur_entree;
+            vect.definir_element(i, valeur_entree);
+        }
+        return in;
+    }
 
-		//on change la dim du vecteur
-		vect.m_dim = dim;
-		vect.m_tab = new T[dim];
+    // Surcharge de l'opérateur [] pour accéder et modifier les composantes.
+    T& operator[](const int position) {
+        // Vérification de la validité de la position.
+        assert(this->dimension_vecteur > position);
+        return *(this->donnees + position);
+    }
 
-		//Boucle pour personnalizer le vecteur
-		for (int i = 0; i < dim; i++) {
-			//on demande a l'utilisateur de rentre la valeur de la i +1 ieme dimension
-			std::cout << "Quel valeur pour la coordonn�e " << i + 1 << std::endl;
-			in >> value;
-			vect.set(i, value);
-		}
-		return in;
-	}
+    Vecteur<T>& operator=(const Vecteur<T>& other) {
+        // Vérification que les dimensions sont compatibles pour l'assignation.
+        assert(this->dimension_vecteur == other.dimension_vecteur);
 
-	//Redefinition de l'operateur [] pour acceder au element et pourvoir les modifier
-	T&  operator[](const int position) {
-		//On verifie d'abord si la postion qu'on souhaite est valide
-		assert(this->m_dim > position);
-		return *(this->m_tab + position);
-	}
+        // Copie des composantes de l'autre vecteur.
+        for (int i = 0; i < this->dimension_vecteur; i++) {
+            this->donnees[i] = other.obtenir_element(i);
+        }
 
-	Vecteur<T>& operator=(const Vecteur<T>& other) {
-		//On verifie d'abord que les deux vecteurs ont les memes dimensions*
-		assert(this->m_dim == other.m_dim);
-
-		//en suit on copie les elements un a un
-		for (int i = 0; i < this->m_dim; i++) {
-			this->m_tab[i] = other.get(i);
-		}
-
-		return *this;
-	}
+        return *this;
+    }
 
 };
 
 template<class T>
-//operateur d'affichage de vecteur
+// Fonction d'affichage du vecteur.
 std::ostream& operator<<(std::ostream& out, const Vecteur<T>& vect) {
-	afficherVecteur(&vect, out);
-	return out;
+    afficher_vecteur(&vect, out);
+    return out;
 }
 
 template<class T>
-Vecteur<T> add(const Vecteur<T>* v1, const Vecteur<T>* v2) {
-	//on verifie d'abord que les deux vecteurs v1 et v2 ont la meme dimension
-	int dim1, dim2;
-	dim1 = v1->dimensions();
-	dim2 = v2->dimensions();
-	assert(dim1 == dim2);
+// Fonction pour additionner deux vecteurs.
+Vecteur<T> additionner_vecteurs(const Vecteur<T>* v1, const Vecteur<T>* v2) {
+    // Vérification que les deux vecteurs ont la même dimension.
+    int dim1, dim2;
+    dim1 = v1->obtenir_dimension();
+    dim2 = v2->obtenir_dimension();
+    assert(dim1 == dim2);
 
-	//on instancie un vecteur de dimension dim1 = dim2
-	Vecteur<T> my_vect(dim1);
+    // Création du vecteur résultat.
+    Vecteur<T> vecteur_resultat(dim1);
 
-	//ensuite on peut faire la somme des deux vecteurs
-	for (int i = 0; i < dim1; i++) {
-		my_vect.set(i, v1->get(i) + v2->get(i));
-	}
-	//on retourne le resulta;t
-	return my_vect;
+    // Calcul de la somme composante par composante.
+    for (int i = 0; i < dim1; i++) {
+        vecteur_resultat.definir_element(i, v1->obtenir_element(i) + v2->obtenir_element(i));
+    }
+    // Retourne le vecteur somme.
+    return vecteur_resultat;
 }
 
 template<class T>
-void afficherVecteur(const Vecteur<T>* v, std::ostream& out) {
-	const int dim = v->dimensions();
-	out << "Vecteur a " << dim << " dimensions: (";
-	for (int i = 0; i < dim; i++) {
-		if (i + 1 == dim) {
-			out << v->get(i) << ")";
-			break;
-		}
-		out << v->get(i) << ", ";
-	}
-	out << std::endl;
+// Fonction utilitaire pour afficher un vecteur.
+void afficher_vecteur(const Vecteur<T>* v, std::ostream& out) {
+    const int dim = v->obtenir_dimension();
+    out << "Vecteur de " << dim << " dimensions : (";
+    for (int i = 0; i < dim; i++) {
+        if (i + 1 == dim) {
+            out << v->obtenir_element(i) << ")";
+            break;
+        }
+        out << v->obtenir_element(i) << ", ";
+    }
+    out << std::endl;
 }
 
 template <typename T,typename U>
+// Opérateur de produit scalaire entre deux vecteurs.
 T operator*(const Vecteur<T>& v1, const Vecteur<U>& v2) {
-	//etant donn� que le produit scalaire n'est valide que pour les types artihmetique
-	//je rechercher sur internet comment verifie qu'on a bien affaire a un type arithmetique
-	//static_assert(std::is_arithmetic<T>::value && std::is_arithmetic<U>::value, "Le produit scalaire est uniquement d�fini pour les types num�riques !");
+    // Le produit scalaire est défini pour des vecteurs de même dimension.
+    assert(v1.obtenir_dimension() == v2.obtenir_dimension());
 
-	assert(v1.dimensions() == v2.dimensions());
-
-	T produit = 0;
-	for (int i = 0; i < v1.dimensions(); i++) {
-		produit += v1.get(i) * v2.get(i);
-	}
-	return produit;
+    T produit_scalaire = 0;
+    for (int i = 0; i < v1.obtenir_dimension(); i++) {
+        produit_scalaire += v1.obtenir_element(i) * v2.obtenir_element(i);
+    }
+    return produit_scalaire;
 }
